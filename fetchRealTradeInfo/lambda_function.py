4598c895b6ac323e47efe2e3eaf4b0b5be4ba3df
조영석 <hyunsoori@nexon.co.kr>
2018-06-27 11:49:35 +0900

    pymysql 으로 변경

--- fetchRealTradeInfo/lambda_function.py
+++ fetchRealTradeInfo/lambda_function.py
@@ -14,8 +14,7 @@ import json
 
 logger = logging.getLogger(__name__)
 
-
-engine = create_engine('mysql://bush_app_user:a1234567@bushdbinstance.csaae2uji9ic.ap-northeast-2.rds.amazonaws.com/bush?charset=utf8mb4')
+engine = create_engine('mysql+pymysql://bush_app_user:a1234567@bushdbinstance.csaae2uji9ic.ap-northeast-2.rds.amazonaws.com/bush?charset=utf8mb4')
 Base = declarative_base()
 
 seoul_area_code = {
@@ -46,66 +45,63 @@ seoul_area_code = {
     11740: "강동구",
 }
 
+
 class AptTradeInfo(Base):
-	__tablename__ = 'apt_trade_info'
-	
-	id = Column(Integer, primary_key=True)
-	key = Column(String, nullable=False)
-	yyyymm = Column(String, nullable=False)
-	days = Column(String, nullable=False)
-	city_code = Column(String, nullable=False)
-	city_name = Column(String, nullable=False)
-	dong_name = Column(String, nullable=False)
-	apt_name = Column(String, nullable=False)
-	floor = Column(Integer, nullable=False)
-	space = Column(String, nullable=True)
-	price = Column(Integer, nullable=False)
-	start_year = Column(String, nullable=True)
-	created_date = Column(DateTime, default=datetime.datetime.now)
-	updated_date = Column(DateTime, nullable=True)
+    __tablename__ = 'apt_trade_info'
+
+    id = Column(Integer, primary_key=True)
+    key = Column(String, nullable=False)
+    yyyymm = Column(String, nullable=False)
+    days = Column(String, nullable=False)
+    city_code = Column(String, nullable=False)
+    city_name = Column(String, nullable=False)
+    dong_name = Column(String, nullable=False)
+    apt_name = Column(String, nullable=False)
+    floor = Column(Integer, nullable=False)
+    space = Column(String, nullable=True)
+    price = Column(Integer, nullable=False)
+    start_year = Column(String, nullable=True)
+    created_date = Column(DateTime, default=datetime.datetime.now)
+    updated_date = Column(DateTime, nullable=True)
 
 
 async def download(area_code, yyyymm):
-    #url = os.environ['API_URL']
-	url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?serviceKey=x%2FeoVcS2U3HDDhBsiIr0JiZsHB6ymm8GO%2FxIwCtrpkMpVgeRmD8IUv2sWjSsc%2B8iJEGVcUKMb5FTJ8QorRMdTg%3D%3D&LAWD_CD={}&DEAL_YMD={}'
-	url = url.format(area_code, yyyymm)
-	async with aiohttp.request('GET', url) as res:
-		result_xml = await res.text()
-		return xmltodict.parse(result_xml)
+    # url = os.environ['API_URL']
+    url = 'http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?serviceKey=x%2FeoVcS2U3HDDhBsiIr0JiZsHB6ymm8GO%2FxIwCtrpkMpVgeRmD8IUv2sWjSsc%2B8iJEGVcUKMb5FTJ8QorRMdTg%3D%3D&LAWD_CD={}&DEAL_YMD={}'
+    url = url.format(area_code, yyyymm)
+    async with aiohttp.request('GET', url) as res:
+        result_xml = await res.text()
+        return xmltodict.parse(result_xml)
 
 
 async def async_download(session, yyyymm):
-	fts = [asyncio.ensure_future(download(area_code, yyyymm)) for area_code in seoul_area_code.keys()]
-	for f in asyncio.as_completed(fts):
-		r = await f
-		for idx, item in enumerate(r['response']['body']['items']['item']):
-			try:
-				key=hashlib.md5(json.dumps(item, sort_keys=True).encode('utf-8')).hexdigest()
-				city_code = item['지역코드']
-				price = int(item['거래금액'].replace(',', ''))
-				apt_trade_info = AptTradeInfo(key=key, yyyymm=yyyymm, days=item['일'], city_code=city_code, city_name=seoul_area_code[int(city_code)], dong_name=item['법정동'], apt_name=item['아파트'], floor=int(item['층']), space=item['전용면적'], price=price, start_year=item['건축년도'])
-				session.add(apt_trade_info)
-				session.commit()
-			except Exception as e:
-				print(e)
-			
-			
-			
-			
-		
+    fts = [asyncio.ensure_future(download(area_code, yyyymm)) for area_code in seoul_area_code.keys()]
+    for f in asyncio.as_completed(fts):
+        r = await f
+        for idx, item in enumerate(r['response']['body']['items']['item']):
+            try:
+                key = hashlib.md5(json.dumps(item, sort_keys=True).encode('utf-8')).hexdigest()
+                city_code = item['지역코드']
+                apt_trade_info = AptTradeInfo(key=key, yyyymm=yyyymm, days=item['일'], city_code=city_code,
+                                              city_name=seoul_area_code[int(city_code)], dong_name=item['법정동'],
+                                              apt_name=item['아파트'], floor=int(item['층']), space=item['전용면적'],
+                                              price=int(item['거래금액'].replace(',', '')), start_year=item['건축년도'])
+                session.add(apt_trade_info)
+                session.commit()
+            except Exception as e:
+                print(e)
+
+
 def lambda_handler(event, context):
-    
-	connection = engine.connect()
-	Session = sessionmaker(bind=engine)
-	session = Session()
-	
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(async_download(session, '201708'))
-	
-	connection.close()
-    
-	
-    
+    connection = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(async_download(session, '201708'))
+
+    connection.close()
+
 
 if __name__ == "__main__":
     lambda_handler(None, None)
